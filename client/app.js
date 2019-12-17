a4443c1a45916a8e22b72e9c3f7b2de08ecbfdd1
Jake Garlick <jacobgarlick@msn.com>
2019-12-17 15:12:01 -0700

    finished submission of info to client side

--- client/app.js
+++ client/app.js
@@ -4,24 +4,33 @@ $("#submit").on("click", function (e) {
     grabGithubRepo();
 });
 
-$("#seach-again").on("click", function (e){
+$("#search-again").on("click", function (e){
     e.preventDefault();
     newSearch();
     $("#map-location").empty();
-})
+});
+
+introSearch = () => {
+    let introSearch = $("#search-again");
+    introSearch.hide();
+}
 
 hideForm = () => {
     let formHide = $("#form");
     let buttonHide = $("#submit");
+    let buttonShow = $("#search-again");
     formHide.hide();
     buttonHide.hide();
+    buttonShow.show();
 };
 
 newSearch = () => {
     let formShow = $("#form");
     let buttonShow = $("#submit");
+    let buttonHide = $("#search-again");
     formShow.show();
     buttonShow.show();
+    buttonHide.hide();
 }
 
 grabGithubRepo = () => {
@@ -36,7 +45,7 @@ grabGithubRepo = () => {
         method: "GET",
     }).then(function (res) {
         let repos = res
-        console.log(repos)
+        console.log(repos.length)
 
         let extraDiv = $("<div>");
         let mapDiv = $("<div>");
@@ -44,12 +53,13 @@ grabGithubRepo = () => {
         for (let i = 0; i < repos.length; i++) {
             if (repos[i].private === false) {
                 let repoName = repos[i].name;
+                let repoLink = repos[i].html_url;
                 let repoSars = repos[i].stargazers_count;
                 let repoWatch = repos[i].watchers_count;
                 let repoCreate = (moment(repos[i].created_at, "YYYY-MM-DD h:mm:ss").format("dddd, MMMM Do, h:mma"));
                 let repoDesc = repos[i].description;
 
-                let allRepoInfo = `<h3>Repo Title: ${repoName}</h3><hr><p>Current Stars: ${repoSars}</p><p>Current Watchers: ${repoWatch}</p><p>Created On ${repoCreate}</p><p>Description: ${repoDesc}</p>`
+            let allRepoInfo = `<div class="all-repos"><h3><a target="_blank" href=${repoLink}>Repo Title: ${repoName}</a></h3><hr><p>Current Stars: ${repoSars}</p><p>Current Watchers: ${repoWatch}</p><p>Created On ${repoCreate}</p><p>Description: ${repoDesc}</p></div>`
 
                 mapDiv.append(allRepoInfo)
             }
@@ -58,3 +68,5 @@ grabGithubRepo = () => {
         $("#map-location").append(extraDiv)
     })
 };
+
+introSearch();
